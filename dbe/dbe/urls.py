f215b336404345f96629f40573d4733c697f4925
gzpgg3x <gzpgg3x@yahoo.com>
2014-12-31 12:38:49 -0500

    whyProfilePictureNotDisplay
    whyProfilePictureNotDisplay

--- dbe/dbe/urls.py
+++ dbe/dbe/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
+# from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.contrib.auth.decorators import login_required as LR
 from dbe.forum.models import *
 from dbe.forum.views import *
+
+# from django.contrib import admin
+# admin.autodiscover()
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +16,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'dbe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),        
 )
 
 urlpatterns += patterns("dbe.forum.views",
@@ -22,6 +28,6 @@ urlpatterns += patterns("dbe.forum.views",
     (r"^new_topic/(?P<dpk>\d+)/$"          , LR(NewTopic.as_view()), {}, "new_topic"),
     (r"^reply/(?P<dpk>\d+)/$"              , LR(Reply.as_view()), {}, "reply"),
     (r"^profile/(?P<mfpk>\d+)/$"           , LR(EditProfile.as_view()), {}, "profile"),
-
-    (r""                                   , Main.as_view(), {}, "forum_main"),
+    (r"^$"                                 , Main.as_view(), {}, "forum_main"),
+    # (r""                                   , Main.as_view(), {}, "forum_main"),
 )
